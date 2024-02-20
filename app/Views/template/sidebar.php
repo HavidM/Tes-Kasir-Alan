b916cda907004c4b22133f74d6fbe1ddeb359080
HavidM <havidmirojnurarif@gmail.com>
2024-02-20 13:51:43 +0700

    first commit

---
+++ app/Views/template/sidebar.php
@@ -0,0 +1,38 @@
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3"><sup</div>
+            </a>
+
+            <hr class="sidebar-divider">
+            <div class="sidebar-heading">
+                Admistrator
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <div class="sidebar-heading">
+                Interface
+            </div>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('Home/tambah_data'); ?>">
+                    <i class="fas fa-fw fa-table"></i>
+                    <span>Tambah Data</span></a>
+            </li>
+->
+            <hr class="sidebar-divider d-none d-md-block">
+
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+        </ul>
---
+++ public/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tes Skill</title>
+	<style type="text/css">
+		img{
+			width: 250px;
+			height: 200px;
+			padding: 10px;
+		}
+		body
+		{
+			background-color: lavender;
+		}
+	</style>
+</head>
+<body>
+		<a href="#"><img src="cumi tepung.jpg"/></a><a href="#"><img src="cumi bakar.jpg"/></a><a href="#"><img src="cumi cabe garam.jpg"/></a><a href="#"><img src="seafood.jpg"/></a><a href="#">
+    <br>
+    <img src="cah kailan.jpg"/></a><a href="#"><img src="tahu goreng.jpg"/></a><a href="#"><img src="cah tauge.jpg"/></a><a href="#"><img src="sayur asem.jpg"/></a>
+    <br>
+    <a href="#"><img src="udang bakar.jpg"/></a><a href="#"><img src="udang saos padang.jpg"/></a><a href="#"><img src="udang mayo.jpg"/></a><a href="#"><img src="jus alpukat.jpg"/></a>
+    <br>
+    <a href="#"><img src="iced tea.jpg"/></a><a href="#"><img src="es jeruk nipis.jpeg"/></a><a href="#"><img src="nasi putih.jpg"/></a><a href="#"><img src="nasi goreng.jpg"/></a>
+</body>
+</html>
